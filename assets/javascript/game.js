b5a80a012aa7b0eed3c200666ff24711559da7d6
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-03 07:09:38 -0400

    linked css and js files to index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+const game = {
+    persons: [
+        "Nirvana", "Boredoms", "Sheryl Crow", "Ice Cube", "Snoop Dogg", "Oasis", "Mariah Carey", "Backstreet Boys"
+    ],
+
+    rightAnswers: [],
+    wrongAnswers: []
+};
