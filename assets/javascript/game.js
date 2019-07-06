ea96413574ab4c4586ab78d9e45408593a61975e
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-06 10:11:35 -0400

    lowercase issue solved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -33,7 +33,7 @@ const singers = [
     name: "Oasis",
     message:
       "We are Oasis were an English rock band formed in Manchester in 1991.",
-    song: { songName: "Wonderwall", songUrl: "", imageUrl: "assets/images/oasis.jpg"},
+    song: { songName: "Wonderwall", songUrl: "assets/audio/Nirvana-Come-As-You-Are.mp3", imageUrl: "assets/images/oasis.jpg"},
   },
   { name: "Mariah", message: "I am Mariah Carey.", song: "Fantasy" },
   {
@@ -58,9 +58,10 @@ let underscores = [];
 let randomIndex = Math.floor(Math.random() * singers.length);
 console.log(randomIndex);
 
-let singerSelected = singers[randomIndex].name;
+let singerSelected = singers[randomIndex].name.toUpperCase();
 
 let score = 0;
+let remainingGuesses = 10;
 
 console.log(singerSelected);
 
@@ -69,10 +70,10 @@ let startGame = () => {
   score = 0;
 };
 
-// generate underscores based on length of singer's name if space generate hyphen
+// generate underscores based on length of singer's name
 
 let generateUnderscores = () => {
-  for (let i = 0; i <= singerSelected.length - 1; i++) {
+  for (let i = 0; i < singerSelected.length; i++) {
     if (singerSelected.charAt(i)) {
       underscores.push("_");
     }
@@ -85,19 +86,40 @@ document.getElementById("current-word").innerHTML = generateUnderscores().join(
 );
 
 document.addEventListener("keypress", event => {
-  let keyChar = String.fromCharCode(event.keyCode);
-  if (singerSelected.indexOf(keyChar) > -1) {
-    rightAnswers.push(keyChar);
-    document.getElementById("current-word").innerHTML = underscores.join(" ");
-    underscores[singerSelected.indexOf(keyChar)] = keyChar;
+
+  let keyChar = String.fromCharCode(event.keyCode).toUpperCase();
+
+  console.log(singerSelected.indexOf(keyChar) + " "+ keyChar);
+
+  if (singerSelected.indexOf(keyChar) >= 0 ) {
+    rightAnswers.splice(keyChar);
+
+    underscores[(singerSelected.indexOf(keyChar))] = keyChar;
+
+    document.getElementById("current-word").textContent = underscores.join(" ");
+
+    console.log(underscores);
+
     if (underscores.join("") == singerSelected) {
-      document.getElementById("message").innerHTML =
-        "You win!!!" + "<br>" + singers[randomIndex].message;
+      document.getElementById("wins").innerHTML = ++score;
+      document.getElementById("message").innerHTML = "<h3>" +
+        "You win!!!"+"</h4>" + "<h5>"+ singers[randomIndex].message + "</h5>";
         document.getElementById("singer-image").setAttribute("src", singers[randomIndex].song.imageUrl);
-      document.getElementById("wins").innerHTML = score++;
+
+        document.getElementById("audio").setAttribute("src", singers[randomIndex].song.songUrl);
+
+        // document.getElementById("audio").play();
+
+
     }
   } else {
+
     wrongAnswers.push(keyChar);
+    document.getElementById("remaining-guesses").textContent = --remainingGuesses;
     document.getElementById("guessed-letters").innerHTML = wrongAnswers;
+
+  }if(remainingGuesses == 0){
+    document.getElementById("message").innerHTML = "<h3>" +
+        "Sorry not a right guess!!!"+"</h4>"
   }
 });
