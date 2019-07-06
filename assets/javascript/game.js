9176d2464363e1b33a55ea42371e54fae4d9526b
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-07-05 20:36:40 -0400

    images added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,52 @@
 const singers = [
   {
     name: "Nirvana",
-    message: "We are American rock band formed in Aberdeen, Washington, in 1987.",
+    message:"We are American rock band formed in Aberdeen, Washington, in 1987.",
     song: {
-      songName:"Come as you are",
-      url: ""
-    }
+      songName: "Come as you are",
+      songUrl: "assets/audio/Nirvana-Come-As-You-Are.mp3",
+      imageUrl: "assets/images/nirvana.jpg"
+    },
   },
   {
-    name: "Boredoms",
-    message: "A rock band from Osaka, Japan.",
-    song: { songName: "96 Teenage Bondage", url: "" }
+    name: "Radiohead",
+    message: "Radiohead are an English rock band formed in Abingdon-on-Thames in 1985.",
+    song: { songName: "Idioteque", songUrl: "", imageUrl: "assets/images/radiohead.jpg" },
   },
   {
     name: "Sheryl",
     message: "I am Sheryl Suzanne Crow.",
-    song: {songName:"Can’t Cry Anymore", url:""}
+    song: { songName: "Can’t Cry Anymore", songUrl: "", imageUrl: "assets/images/sheryl.jpg" },
   },
   {
     name: "Ricky",
-    message:'Enrique "Ricky" Martín Morales (born December 24, 1971) is a Puerto Rican singer, actor and author.',
-    song: {songName:"Livin' la Vida Loca", url:""}
+    message:
+      'Enrique "Ricky" Martín Morales (born December 24, 1971) is a Puerto Rican singer, actor and author.',
+    song: { songName: "Livin' la Vida Loca", songUrl: "", imageUrl: "assets/images/ricky-martin.jpg"},
   },
   {
     name: "Britney",
     message: "I am Britney Spears.",
-    song: {songName:"Baby One More Time", url:""}
+    song: { songName: "Baby One More Time", songUrl: "", imageUrl: "assets/images/britney.jpg" },
   },
   {
     name: "Oasis",
-    message:"We are Oasis were an English rock band formed in Manchester in 1991.",
-    song: {songName:"Wonderwall", url:""}
+    message:
+      "We are Oasis were an English rock band formed in Manchester in 1991.",
+    song: { songName: "Wonderwall", songUrl: "", imageUrl: "assets/images/oasis.jpg"},
   },
   { name: "Mariah", message: "I am Mariah Carey.", song: "Fantasy" },
   {
     name: "Shania",
-    message: "Shania Twain,(born Eilleen Regina Edwards; August 28, 1965) is a Canadian singer, songwriter and actress. ",
-    song: {songName: "You’re Still the One", url:""}
+    message:
+      "Shania Twain,(born Eilleen Regina Edwards; August 28, 1965) is a Canadian singer, songwriter and actress. ",
+    song: { songName: "You’re Still the One", songUrl: "", imageUrl: "assets/images/mariah.png" },
   },
   {
     name: "Beck",
-    message: "Beck, is an American singer-songwriter, musician, and record producer.",
-    song: {songName:"Pay No Mind (Snoozer)", url:""}
+    message:
+      "Beck, is an American singer-songwriter, musician, and record producer.",
+    song: { songName: "Pay No Mind (Snoozer)", songUrl: "", imageUrl: "assets/images/beck.jpeg" },
   },
 ];
 
@@ -88,6 +93,7 @@ document.addEventListener("keypress", event => {
     if (underscores.join("") == singerSelected) {
       document.getElementById("message").innerHTML =
         "You win!!!" + "<br>" + singers[randomIndex].message;
+        document.getElementById("singer-image").setAttribute("src", singers[randomIndex].song.imageUrl);
       document.getElementById("wins").innerHTML = score++;
     }
   } else {
